d034d7ef57bb0c2742a22c206f48c2b1bb78802c
Carlos Blanco <carlosblanco@me.com>
2012-12-18 10:06:40 +0100

    Flickr Search
    Flickr Search

---
+++ js/script.js
@@ -0,0 +1,19 @@
+$(function(){
+		$("#button").on("click", function(e){
+			e.preventDefault();
+			var tags = $("#search").val();
+			$.getJSON("http://api.flickr.com/services/feeds/photos_public.gne?jsoncallback=?",{
+				tags: tags,
+				format: "json",
+				tagmode: "any"
+			}, todoBien);
+		});
+		function todoBien(dato){
+			$("#resultado").empty();
+			$(dato.items).each(function(indice, valor){
+				var urlImagen = valor.media.m;
+				console.log (urlImagen);
+				$('<img src="' + urlImagen + '" />').appendTo("#resultado");
+			});
+		}
+	});
